a49ec072b16aec48a917f28b0dd14f585f4cdd8b
ezntek <eason@ezntek.com>
2023-09-25 09:37:48 +0800

    progress(cli): add ability to run builds

--- smbuilder-cli/src/main.rs
+++ smbuilder-cli/src/main.rs
@@ -1,17 +1,78 @@
-use std::{path::PathBuf, thread};
-
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use colored::Colorize;
-use smbuilder::prelude::*;
+use smbuilder::prelude::{callback_types::LogType, *};
+use std::{fmt::format, path::PathBuf, thread};
+
+use smbuilder_cli::get_builder;
+
+#[derive(Clone, Subcommand)]
+enum Action {
+    Build { spec_path: PathBuf },
+    Run { spec_path: PathBuf },
+}
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    base_dir: PathBuf,
+    #[command(subcommand)]
+    subcommand: Action,
 }
 
-fn get_builder<'b>(base_dir: PathBuf) -> Builder<'b> {
-    let mut callbacks = Callbacks::new()
+fn build(base_dir: PathBuf, callbacks: Callbacks<'static>) {
+    if !base_dir.is_dir() {
+        panic!("{} is not a directory! please enter the path to a directory with an `build.yaml` in the root of it.", base_dir.display());
+    }
+
+    let mut builder = get_builder(base_dir.clone(), callbacks);
+
+    thread::spawn(move || match builder.build() {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("{}", e.to_string());
+            std::process::exit(1);
+        }
+    })
+    .join()
+    .unwrap();
+}
+
+fn run(base_dir: PathBuf, mut callbacks: Callbacks) {
+    let spec = Spec::from_file_checked(base_dir.join("build.yaml"), &mut callbacks);
+    let spec = match spec {
+        Ok(s) => s,
+        Err(e) => {
+            if let Some(cb) = &mut callbacks.log_cb {
+                cb(
+                    LogType::Error,
+                    format!("failed to open the specfile: {}", e).as_str(),
+                );
+            }
+            std::process::exit(1)
+        }
+    };
+    let region = spec.rom.region.to_string();
+    let path = base_dir
+        .join(&spec.repo.name)
+        .join("build")
+        .join(format!("{}_pc", &region))
+        .join(format!("sm64.{}.f3dex2e", &region));
+    let mut cmd = std::process::Command::new(path);
+    let mut child = cmd.spawn().unwrap_or_else(|e| {
+        // FIXME: integrate into log
+        if let Some(cb) = &mut callbacks.log_cb {
+            cb(LogType::Error, "failed to spawn the command");
+        };
+        std::process::exit(1);
+    });
+    child.wait().unwrap(); // TODO: macro out the errors
+}
+
+fn main() {
+    color_eyre::install().unwrap();
+
+    let args = Args::parse();
+
+    let callbacks = Callbacks::new()
         .log(|log_type, text| {
             use callback_types::LogType as L;
             match log_type {
@@ -35,35 +96,10 @@ fn get_builder<'b>(base_dir: PathBuf) -> Builder<'b> {
             println!("{}{}", "stage: ".bold().green(), stage.to_string());
         });
 
-    let spec_path = base_dir.join("build.yaml");
-    let spec = Spec::from_file_checked(spec_path, &mut callbacks).unwrap();
-    Builder::new(spec, base_dir.clone(), callbacks).unwrap()
-}
-
-fn build<'b>(mut builder: Builder<'b>) {
-    match builder.build() {
-        Ok(_) => (),
-        Err(e) => {
-            eprintln!("{}", e.to_string());
-            std::process::exit(1);
-        }
+    match args.subcommand {
+        Action::Build {
+            spec_path: base_dir,
+        } => build(base_dir, callbacks),
+        Action::Run { spec_path } => run(spec_path, callbacks),
     };
-}
-
-fn main() {
-    color_eyre::install().unwrap();
-
-    let args = Args::parse();
-
-    if !args.base_dir.is_dir() {
-        panic!("{} is not a directory! please enter the path to a directory with an `build.yaml` in the root of it.", args.base_dir.display());
-    }
-
-    let builder = get_builder(args.base_dir);
-
-    thread::spawn(move || {
-        build(builder);
-    })
-    .join()
-    .unwrap();
 }
